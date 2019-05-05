88c946cc3bceafc62a3f9eb518c04f711bf7e7c7
themanyone <nospam@thenerdshow.com>
2019-05-04 21:08:30 -0800

    og:image fixes

--- views/default/page/elements/html.php
+++ views/default/page/elements/html.php
@@ -27,22 +27,31 @@ $head_content = elgg_extract('head', $vars, '');
 $body_content = elgg_extract('body', $vars, '');
 
 //get page title, description
-preg_match('#<title>(.*?)</title#', $head_content, $title);
-preg_match('#<meta name="description" content="(.*?)"#', $head_content, $description);
+if ($q = mb_ereg_replace("\s+", "-", trim($_GET['q']))){
+	$title = $q.".".$_SERVER["HTTP_HOST"];
+} else {
+	preg_match('#<title>(.*?)</title#', $head_content, $title);
+	$title = $title[1];
+}
+preg_match('#<meta name="description"\s+content="(.*?)"#', $head_content, $description);
+$description = $description[1];
 
 //check whether we are in fileview (workaround for Facebook not showing images)
 preg_match('#/file/view/(\d+)#', $_SERVER['REQUEST_URI'], $fileview);
 
 //add Open Graph protocol headers for page url, title & description
 $head_content.="<meta property=\"og:url\" content=\"https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]\">\n";
-$head_content.="<meta property=\"og:title\" content=\"$title[1]\">\n";
-$head_content.="<meta property=\"og:description\" content=\"$description[1]\">\n";
+$head_content.="<meta property=\"og:title\" content=\"$title\">\n";
+$head_content.="<meta property=\"og:description\" content=\"$description\">\n";
 
 //get image URLs except profile pic
-preg_match_all('/<img.*?src="([^"]+)/',$body_content,$srcs);
-if($fileview[1]){
+preg_match_all('/<img.*?src="([^"]+)/', $body_content, $srcs);
+if($fileview[1]){ // fix wrong URL in file view
 	$srcs = Array("$_SERVER[HTTP_HOST]/serve-icon/$fileview[1]/large");
-} else $srcs = preg_grep('/small\....$/', $srcs[1], PREG_GREP_INVERT);
+} else {
+	$srcs = preg_grep('/small\....$/', $srcs[1], PREG_GREP_INVERT); // ignore small icons
+	$srcs = preg_grep('/fu32\.png$/', $srcs, PREG_GREP_INVERT); // remove site logo
+}
 
 //make og:image headers for images, except profile pic
 foreach ($srcs as $ssl_src) {
